0c4d3851911ecdb594095572bcf8ae68676e6660
luischs23 <luisc.herreras@gmail.com>
2025-04-04 11:52:03 -0500

    pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,41 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind'; 
 import vercel from '@astrojs/vercel'; 
+import pwa from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    pwa({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Astro Inventory',
+        short_name: 'Inventory',
+        description: 'Gestor de inventario con Astro',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo3.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo3.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/logo3.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
+  
+    react(), tailwind()],
   prefetch: {
     defaultStrategy: 'hover', // o 'tap' para móviles
     prefetchAll: true // precarga todos los enlaces
